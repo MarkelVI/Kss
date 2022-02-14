fefc8e57c2f9799775c495e4a43dab7536c8ec1a
MarkelVI <99678734+MarkelVI@users.noreply.github.com>
2022-02-14 22:48:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ function getVal() {
   var chap = val.replace(/http\S+/g, "");
   var kss = (chap.length/1000).toFixed(1)
   var kas = (val.length/1000).toFixed(1)
-  var taux = (parseFloat(kss) / parseFloat(kas) *100).toFixed()
-  console.log(kas.length + kss)
+  var taux = parseInt(parseFloat(kss) / parseFloat(kas) *100)
   
   document.getElementById('answer').innerHTML =`Le chapitrent fait ${kss} kss\net a un taux de stickerisation de ${taux} %`;
 }
