499c9e04ee3f009637294f8f0d3bb398102d8dd6
MarkelVI <99678734+MarkelVI@users.noreply.github.com>
2022-02-14 23:23:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function getVal() {
   var chap = val.replace(/http\S+/g, "");
   var kss = (chap.length/1000).toFixed(1)
   var kas = (val.length/1000).toFixed(1)
-  var taux = parseInt(parseFloat(kss) / parseFloat(kas) *100)
+  var sticker = (val.match(/http\S+/g) || []).length
   
-  document.getElementById('answer').innerHTML =`Le chapitrent fait ${kss} kss\net a un taux de stickerisation de ${taux} %`;
+  document.getElementById('answer').innerHTML =`Le chapitrent fait ${kss} kss, ${kas} kas\net ${sticker} stickers`;
 }
