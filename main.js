968c22750b2f4ac3d4060a997882e0a0d78736bb
MarkelVI <99678734+MarkelVI@users.noreply.github.com>
2022-08-30 23:54:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function getVal() {
   const val = document.getElementById('longueur').value;
   var chap = val.replace(/http\S+/g, "");
-  var kss = (chap.length/1000).toFixed(1)
-  var kas = (val.length/1000).toFixed(1)
+  var kss = (chap.length/1000*2).toFixed(1)
+  var kas = (val.length/1000*2).toFixed(1)
   var sticker = (val.match(/http\S+/g) || []).length
   
   document.getElementById('answer').innerHTML =`Le chapitrent fait ${kss} kss, ${kas} kas\net ${sticker} stickers`;
